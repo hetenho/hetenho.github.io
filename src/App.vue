2c7b1e6deeab5b6f51c96e61d497e71a703ad2d4
Henrik Tenhovirta <henrik.tenhovirta@checkout.fi>
2020-01-15 21:54:04 +0200

    put vue 3 in

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0)
+    const inc = () => {
+      count.value++
+    }
+
+    return {
+      count,
+      inc
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
