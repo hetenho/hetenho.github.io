bbffb1312b4ad7f0c6cf0f76aa14489f7e3b0240
Henrik Tenhovirta <henrik.tenhovirta@checkout.fi>
2020-01-16 08:52:03 +0200

    use styles and stuff

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <h1>Hello!</h1>
+    <nav>
+      <span class="logo">FEELS</span>
+      <ul class="menu">
+        <li><a href='/contact'>Contact</a></li>
+        <li><a href='/projects'>Projects</a></li>
+      </ul>
+    </nav>
+    <div class="centered">
+      <h1 class="animation">A reliable Frontend Agency that gives you the feels.</h1>
+      <button>Contact</button>
+    </div>
   </div>
 </template>
 
@@ -23,7 +33,96 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-family: Arial, Helvetica, sans-serif;
-}
+
+  nav {
+    padding: 0em 2em;
+    display: flex;
+  }
+
+  .menu {
+    margin: 0;
+    margin-left: auto;
+    padding: 0;
+    display: flex;
+  }
+
+  .menu li {
+    list-style: none;
+    display: flex;
+  }
+
+  .menu a {
+    color: rgb(255, 2, 149);
+    padding: 0.5em;
+    align-self: center;
+    display: flex;
+    text-decoration: none;
+    font-size: 14px;
+  }
+
+  .menu a:hover {
+    text-decoration: underline;
+  }
+
+  .logo {
+    font-family: 'Kanit', sans-serif;
+    font-size: 20px;
+    background: -webkit-linear-gradient(rgb(255, 2, 149),rgb(194, 38, 255));
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-style: italic;
+    padding: 1em 1em 1em 0;
+  }
+
+  .centered {
+    text-align: center;
+    padding: 0 2em;
+    max-width: 440px;
+    margin: 15% auto 0;
+  }
+
+  h1 {
+    font-size: 24px;
+  }
+
+  button {
+    background: rgb(255, 2, 149);
+    color: white;
+    border-radius: 6px;
+    box-shadow: none;
+    border: none;
+    font-size: 16px;
+    padding: 0.5em 1em;
+  }
+
+  @media (min-width: 640px) {
+    h1 {
+      font-size: 32px;
+    }
+
+    .centered {
+      padding: 0 4em;
+      max-width: 1200px;
+    }
+
+    button {
+      font-size: 20px;
+      padding: 0.5em 1.5em;
+    }
+  }
+
+  /* Animation */
+  .animation{
+    animation: typewriter 4s steps(44) 1s 1 normal both,
+              blinkTextCursor 500ms steps(44) infinite normal;
+  }
+  @keyframes typewriter{
+    from{width: 0;}
+    to{width: 24em;}
+  }
+  @keyframes blinkTextCursor{
+    from{border-right-color: rgba(253, 1, 220, 0.75);}
+    to{border-right-color: transparent;}
+  }
 </style>
